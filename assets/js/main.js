4e45b496e7b4d57bba49a9e61867cba251e912e5
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2023-04-21 07:40:44 +0000

    n

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('#btnMenu').click(function(){
       $(this).toggleClass('bi-x-lg');
       $('.navbar').toggleClass('active');
-      $('.header').css({'background':'#8a57b6','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+      $('.header-home').css({'background':'#8a57b6','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
     });
   
     $(window).on('load scroll',function(){
@@ -12,9 +12,9 @@ $(document).ready(function(){
       $('.navbar').removeClass('nav-toggle');
   
       if($(window).scrollTop() > 30){
-        $('.header').css({'background':'#8a57b6','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+        $('.header-home').css({'background':'#8a57b6','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
       }else{
-        $('.header').css({'background':'none','box-shadow':'none'});
+        $('.header-home').css({'background':'none','box-shadow':'none'});
       }
     });
   
